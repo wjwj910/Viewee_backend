611e0d0d8ee42db860a73ab6efcbea4234d65bdd
wjlee <wjwj910@gmail.com>
2023-09-19 02:41:55 +0900

    commit

--- src/test/java/digginger/Viewee/VieweeApplicationTests.java
+++ src/test/java/digginger/Viewee/VieweeApplicationTests.java
@@ -1,13 +1,8 @@
 package digginger.Viewee;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VieweeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
