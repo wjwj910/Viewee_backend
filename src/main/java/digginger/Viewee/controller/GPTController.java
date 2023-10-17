623158f86f1d31cd9e00a499e79c7c665befe9c8
wjlee <wjwj910@gmail.com>
2023-10-17 17:11:31 +0900

    modify controller and prompt

--- src/main/java/digginger/Viewee/controller/GPTController.java
+++ src/main/java/digginger/Viewee/controller/GPTController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -94,7 +95,7 @@ public class GPTController {
         return ResponseEntity.ok(apiResponseDTO.getResponseMsg());
     }
 
-    @PostMapping("gpt/re_Interview")
+    @GetMapping("gpt/re_Interview")
     public ResponseEntity<?> re_Interview(@RequestBody interviewDTO interviewDTO){
 
         GPT gpt = new GPT(GPT.GPT3_5, GPT.RE_INTERVIEW, GPT.OPEN_API_KEY);
